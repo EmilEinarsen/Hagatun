9be72976a39c8453782d10c7e7c8513e80db2697
EmilEinarsen <emil@hagatun.se>
2023-04-24 09:02:21 -0400

    revert(remix.config.js): :rewind: unstable_dev

--- remix.config.js
+++ remix.config.js
@@ -5,20 +5,10 @@ module.exports = {
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js"
   future: {
-    unstable_dev: true,
     unstable_tailwind: true,
     v2_routeConvention: true,
     v2_meta: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true
-  },
-
-  // serverBuildTarget vercel
-  // https://remix.run/docs/en/1.15.0/pages/v2#serverbuildtarget
-  publicPath: "/build/",
-  serverBuildPath: "api/index.js",
-  serverMainFields: ['main', 'module'],
-  serverModuleFormat: "cjs",
-  serverPlatform: "node",
-  serverMinify: false,
+  }
 };
